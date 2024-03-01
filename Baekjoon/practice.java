9804e806a14173f4fd071af23b6a06cdbf9073ae
chanyoungit <uniti0903@naver.com>
2024-03-01 20:58:04 +0900

    commit message

---
+++ Baekjoon/practice.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class practice {
+    public static void main(String[] args) {
+        ArrayList<Integer>[] A = new ArrayList[3];
+        System.out.println(A.length);
+    }
+}
