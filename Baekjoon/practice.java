f4806f9e07fb571853068ab05f6e84dcee2cc50b
chanyoungit <uniti0903@naver.com>
2024-03-03 21:43:38 +0900

    commit message

--- Baekjoon/practice.java
+++ Baekjoon/practice.java
@@ -8,9 +8,9 @@ import java.util.StringTokenizer;
 public class practice {
     static int[] dx = {0, 1, 0, -1};
     static int[] dy = {1, 0, -1, 0};
-    static int N, M;
     static boolean[][] visited;
     static int[][] A;
+    static int N, M;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,8 +21,8 @@ public class practice {
         visited = new boolean[N][M];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            String str = st.nextToken();
-            for (int j = 0; j < M; j++) A[i][j] = Integer.parseInt(str.substring(j, j + 1));
+            String line = st.nextToken();
+            for (int j = 0; j < M; j++) A[i][j] = Integer.parseInt(line.substring(j, j + 1));
         }
         BFS(0, 0);
         System.out.println(A[N - 1][M - 1]);
@@ -30,17 +30,17 @@ public class practice {
 
     public static void BFS(int i, int j) {
         Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{i, j});
+        queue.offer(new int[]{i, j});
         while (!queue.isEmpty()) {
-            int[] num = queue.poll();
+            int now[] = queue.poll();
             visited[i][j] = true;
             for (int k = 0; k < 4; k++) {
-                int x = num[0] + dx[k];
-                int y = num[1] + dy[k];
+                int x = now[0] + dx[k];
+                int y = now[1] + dy[k];
                 if (x >= 0 && y >= 0 && x < N && y < M) {
-                    if (!visited[x][y] && A[x][y] != 0) {
+                    if (A[x][y] != 0 && !visited[x][y]) {
                         visited[x][y] = true;
-                        A[x][y] = A[num[0]][num[1]] + 1;
+                        A[x][y] = A[now[0]][now[1]] + 1;
                         queue.add(new int[]{x, y});
                     }
                 }
