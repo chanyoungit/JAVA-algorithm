c00519c839525527b4ca20e07afc8047ad855fbb
chanyoungit <uniti0903@naver.com>
2024-03-10 13:38:32 +0900

    commit message

--- Baekjoon/practice.java
+++ Baekjoon/practice.java
@@ -1,14 +1,31 @@
 import java.io.*;
-import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class practice {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        bw.close();
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        char[] ch = br.readLine().toCharArray();
+//        int count = 0;
+//        for (int i = 0; i < ch.length; i++) {
+//            count++;
+//            if (ch.length == 1) break;
+//            if (i <= ch.length - 2) {
+//                if (ch[i] == 'c' && ch[i + 1] == '=') count--;
+//                else if (ch[i] == 'c' && ch[i + 1] == '-') count--;
+//                else if (ch[i] == 'd' && ch[i + 1] == '-') count--;
+//                else if (ch[i] == 'l' && ch[i + 1] == 'j') count--;
+//                else if (ch[i] == 'n' && ch[i + 1] == 'j') count--;
+//                else if (ch[i] == 's' && ch[i + 1] == '=') count--;
+//                else if (ch[i] == 'z' && ch[i + 1] == '=') count--;
+//            } else if (i <= ch.length - 3) {
+//                if (ch[i] == 'd' && ch[i + 1] == 'z' && ch[i + 2] == '=') count -= 2;
+//            }
+//            System.out.println(count);
+//        }
+//        bw.write(count + "");
+//        bw.close();
+        String s= "hello";
+        System.out.println(s.length());
     }
 }
