63c93f3f10c7e109ed8ddfa2985e90b784007366
chanyoungit <uniti0903@naver.com>
2024-03-11 23:12:11 +0900

    commit message

--- Baekjoon/practice.java
+++ Baekjoon/practice.java
@@ -1,29 +1,36 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class practice {
+    static PriorityQueue<Long> queue;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-        int[][] A = new int[101][101];
-        int width = 0;
-        int N = Integer.parseInt(br.readLine());
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
-            for (int j = s + 1; j <= s + 10; j++) {
-                if (j >= 101) break;
-                for (int k = e + 1; k <= e + 10; k++) {
-                    if (k >= 101) break;
-                    if (A[j][k] == 1) continue;
-                    A[j][k] = 1;
-                    width++;
-                }
+        long N = Long.parseLong(br.readLine());
+        queue = new PriorityQueue<>((o1, o2) -> Long.compare(o1, o2));
+        so(N);
+        while (!queue.isEmpty()) {
+            bw.write(queue.poll() + "\n");
+        }
+        bw.close();
+    }
+
+    public static void so(long a) {
+        boolean bl = true;
+        for (int i = 2; i < a; i++) {
+            if (a % i == 0) {
+                bl = false;
+                queue.add((long) i);
+                long b = a / i;
+                so(b);
+                break;
             }
         }
-        bw.write(width + "");
-        bw.close();
+        if (bl) {
+            queue.add(a);
+        }
     }
 }
---
+++ Baekjoon/practice1.java
@@ -0,0 +1,6 @@
+public class practice1 {
+    public static void main(String[] args) {
+        double C = 0.04 % 0.01;
+        System.out.println(C);
+    }
+}
