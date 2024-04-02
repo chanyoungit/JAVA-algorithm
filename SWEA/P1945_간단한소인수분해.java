e9da2d992fc3ec25b6c3ee6a32862c025984d635
chanyoungit <uniti0903@naver.com>
2024-04-02 20:48:48 +0900

    commit message

--- SWEA/P1945_간단한소인수분해.java
+++ SWEA/P1945_간단한소인수분해.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class P1945_간단한소인수분해 {
+import java.io.*;
+
+public class P1945_간단한소인수분해 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            int N = Integer.parseInt(br.readLine());
+            int a = 0, b = 0, c = 0, d = 0, e = 0;
+
+            while (N % 2 == 0) {
+                N /= 2;
+                a++;
+            }
+            while (N % 3 == 0) {
+                N /= 3;
+                b++;
+            }
+            while (N % 5 == 0) {
+                N /= 5;
+                c++;
+            }
+            while (N % 7 == 0) {
+                N /= 7;
+                d++;
+            }
+            while (N % 11 == 0) {
+                N /= 11;
+                e++;
+            }
+
+            int number = i + 1;
+
+            System.out.println("#" + number + " " + a + " " + b + " " + c + " " + d + " " + e);
+        }
+    }
 }
