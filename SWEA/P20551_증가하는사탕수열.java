c6efbe276e886bdb2fa7bcefac00dee65aa63e39
chanyoungit <uniti0903@naver.com>
2024-04-02 21:48:09 +0900

    commit message

--- SWEA/P20551_증가하는사탕수열.java
+++ SWEA/P20551_증가하는사탕수열.java
@@ -1,2 +1,53 @@
-package PACKAGE_NAME;public class P20551_증가하는사탕수열 {
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class P20551_증가하는사탕수열 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            boolean bl = true;
+            int count = 0;
+            int index = i + 1;
+
+            while (b >= c) {
+                b--;
+                if (b == 0 || b == 1) {
+                    bw.write("#" + index + " " + "-1" + "\n");
+                    bl = false;
+                    break;
+                }
+                count++;
+            }
+
+            if (bl) {
+                while (a >= b) {
+                    a--;
+                    if (a == 0) {
+                        bw.write("#" + index + " " + "-1" + "\n");
+                        bl = false;
+                        break;
+                    }
+                    count++;
+                }
+            }
+
+            if (bl)
+                bw.write("#" + index + " " + count + "\n");
+
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
