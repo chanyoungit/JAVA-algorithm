1dc25508a472767b179e15921d8cf5c00869ad8e
chanyoungit <uniti0903@naver.com>
2024-04-08 20:38:30 +0900

    commit message

--- SWEA/P13732_정사각형판정.java
+++ SWEA/P13732_정사각형판정.java
@@ -2,10 +2,6 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class P13732_정사각형판정 {
-    static boolean[][] visit;
-    static boolean[][] visit2;
-    static int N;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -13,73 +9,51 @@ public class P13732_정사각형판정 {
 
         int T = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < T; i++) {
-            N = Integer.parseInt(br.readLine());
-            visit = new boolean[N][N];
-            visit2 = new boolean[N][N];
+        for (int test_case = 1; test_case <= T; test_case++) {
+            int N = Integer.parseInt(br.readLine());
+            char[][] arr = new char[N][N];
+
+            for (int i = 0; i < N; i++) {
+                arr[i] = br.readLine().toCharArray();
+            }
+
+            int[] index = { N, N, -1, -1 };
+
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (arr[i][j] == '#') {
+                        index[0] = Math.min(index[0], i); // 최소 x의 값
+                        index[1] = Math.min(index[1], j); // 최소 y의 값
+                        index[2] = Math.max(index[2], i); // 최대 x의 값
+                        index[3] = Math.max(index[3], j); // 최대 y의 값
+                    }
+                }
+            }
+
             boolean bl = true;
-            int count = 0;
-
-            for (int j = 0; j < N; j++) {
-                String str = br.readLine();
-                char[] ch = str.toCharArray();
-
-                for (int k = 0; k < N; k++) {
-                    if (ch[k] == '#') {
-                        visit[j][k] = true;
-                    }
-                }
-            }
-
-            for (int j = 0; j < N; j++) {
-                for (int k = 0; k < N; k++) {
-                    if (visit[j][k] && !visit2[j][k]) {
-                        if(bl){
-                            bl = DFS(j, k);
-                            count++;
+            if (index[2] - index[0] == index[3] - index[1]) {
+                for (int i = index[0]; i <= index[2]; i++) {
+                    for (int j = index[1]; j <= index[3]; j++) {
+                        if (arr[i][j] != '#') {
+                            bl = false;
+                            break;
                         }
+                        if (!bl)
+                            break;
                     }
                 }
-            }
-            int seq = i + 1;
-            if (bl && count == 1) {
-                bw.write("#" + seq + " yes\n");
-            } else {
-                bw.write("#" + seq + " no\n");
-            }
+            } else
+                bl = false;
+
+            if (bl)
+                bw.write("#" + test_case + " yes\n");
+            else
+                bw.write("#" + test_case + " no\n");
+
         }
 
         bw.flush();
         bw.close();
         br.close();
-    }
-
-    public static boolean DFS(int a, int b) {
-        int xcount = 0;
-
-        int bm = b + 1;
-        while (true) {
-            if (bm < N) {
-                if (!visit[a][bm]) {
-                    break;
-                }
-                visit2[a][bm] = true;
-                bm++;
-                xcount++;
-            } else
-                break;
-
-        }
-
-        for (int i = a + 1; i <= a + xcount; i++) {
-            for (int j = b; j <= b + xcount; j++) {
-                System.out.println("i = " + i + " j = " + j);
-                if (!visit[i][j]) {
-                    return false;
-                }
-                visit2[i][j] = true;
-            }
-        }
-        return true;
     }
 }
--- SWEA/P14413_격자판칠하기.java
+++ SWEA/P14413_격자판칠하기.java
@@ -10,65 +10,56 @@ public class P14413_격자판칠하기 {
 
         int T = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < T; i++) {
+        for (int test_case = 1; test_case <= T; test_case++) {
             st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
             int M = Integer.parseInt(st.nextToken());
-            int[][] arr = new int[N][M];
 
-            for (int j = 0; j < N; j++) {
-                String str = br.readLine();
-                char[] ch = str.toCharArray();
+            char[][] arr = new char[N][M];
 
-                for (int k = 0; k < ch.length; k++) {
-                    if (ch[k] == '#') {
-                        arr[j][k] = 0;
-                    } else if (ch[k] == '.') {
-                        arr[j][k] = 1;
-                    } else {
-                        arr[j][k] = -1;
-                    }
-                }
+            for (int i = 0; i < N; i++) {
+                arr[i] = br.readLine().toCharArray();
             }
 
-            int value = -1;
-
-            for (int j = 0; j < N; j++) {
-                for (int k = 0; k < M; k++) {
-                    if (arr[j][k] != -1) {
-                        value = arr[j][k];
-                        break;
-                    }
-                    if (value != -1)
-                        break;
-                }
-            }
-
-
-            boolean bl = true;
-            for (int j = 0; j < N; j++) {
-                for (int k = 0; k < M; k++) {
-                    if (arr[j][k] != value && arr[j][k] != -1) {
-                        bl = false;
-                        break;
-                    }
-                    value = (value + 1) % 2;
-                }
-                if (M % 2 == 0) {
-                    value = (value + 1) % 2;
-                }
-            }
-
-            int seq = i + 1;
-            if (bl) {
-                bw.write("#" + seq + " possible\n");
+            boolean bl;
+            if (arr[0][0] == '#') {
+                bl = check(arr, '#');
+            } else if (arr[0][0] == '.') {
+                bl = check(arr, '.');
             } else {
-                bw.write("#" + seq + " impossible\n");
+                bl = check(arr,'#') ? true : check(arr, '.');
             }
+
+            if(bl)
+                bw.write("#" + test_case + " possible\n");
+            else
+                bw.write("#" + test_case + " impossible\n");
+
         }
 
         bw.flush();
         bw.close();
         br.close();
+    }
+
+    public static boolean check(char[][] arr, char start) {
+        char evenSum = start;
+        char oddSum;
+        if (evenSum == '#') {
+            oddSum = '.';
+        } else {
+            oddSum = '#';
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if ((i + j) % 2 == 0 && arr[i][j] == oddSum) {
+                    return false;
+                } else if ((i + j) % 2 == 1 && arr[i][j] == evenSum) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
