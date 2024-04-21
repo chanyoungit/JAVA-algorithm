c15b24545382952184b32b9b3bbc56f6d47c9bba
chanyoungit <uniti0903@naver.com>
2024-04-22 00:41:41 +0900

    commit message

--- SWEA/NOTE.java
+++ SWEA/NOTE.java
@@ -1,7 +1,4 @@
 import java.io.*;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
 
 public class NOTE {
 
@@ -9,13 +6,5 @@ public class NOTE {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int M = 10;
-
-        while (M != 0) {
-            int a = M & 1;
-            System.out.println(a);
-            M = M >> 1;
-            System.out.println(M);
-        }
     }
 }
