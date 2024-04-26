e747996687e842ef2a962afcbe5210d7b8971e9d
chanyoungit <uniti0903@naver.com>
2024-04-26 14:53:50 +0900

    commit message

--- Programmers/최소직사각형.java
+++ Programmers/최소직사각형.java
@@ -1,17 +1,14 @@
 public class 최소직사각형 {
     public int solution(int[][] sizes) {
-        for (int i = 0; i < sizes.length; i++) {
-            if (sizes[i][0] < sizes[i][1]) {
-                int temp = sizes[i][1];
-                sizes[i][1] = sizes[i][0];
-                sizes[i][0] = temp;
-            }
-        }
-
         int x = 0;
         int y = 0;
-
         for (int i = 0; i < sizes.length; i++) {
+            if (sizes[i][0] < sizes[i][1]) {
+                int temp = sizes[i][0];
+                sizes[i][0] = sizes[i][1];
+                sizes[i][1] = temp;
+            }
+
             if (x < sizes[i][0]) {
                 x = sizes[i][0];
             }
@@ -21,7 +18,7 @@ public class 최소직사각형 {
             }
         }
 
-        int result = x * y;
-        return result;
+        int answer = x * y;
+        return answer;
     }
 }
--- Programmers/피로도.java
+++ Programmers/피로도.java
@@ -1,21 +1,24 @@
 public class 피로도 {
     static boolean[] visited;
-    static int count = 0;
+    static int answer = 0;
 
     public int solution(int k, int[][] dungeons) {
         visited = new boolean[dungeons.length];
         dfs(dungeons, k, 0);
-        return count;
+        return answer;
     }
 
-    public static void dfs(int[][] dungeons, int k, int depth) {
+    public void dfs(int[][] dungeons, int k, int cnt) {
+
         for (int i = 0; i < dungeons.length; i++) {
             if (!visited[i] && k >= dungeons[i][0]) {
                 visited[i] = true;
-                dfs(dungeons, k - dungeons[i][1], depth + 1);
+                dfs(dungeons, k - dungeons[i][1], cnt + 1);
                 visited[i] = false;
             }
-            count = Math.max(count, depth);
         }
+
+        answer = Math.max(answer, cnt);
+        return;
     }
 }
