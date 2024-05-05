8e93938b7e868b792e34c908e7a9dad06cc4d782
chanyoungit <uniti0903@naver.com>
2024-05-06 00:58:35 +0900

    commit message

--- SWEA/NOTE.java
+++ SWEA/NOTE.java
@@ -1,5 +1,8 @@
+import java.io.*;
+
 class NOTE {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader bw = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter br = new BufferedWriter(new OutputStreamWriter(System.out));
     }
 }
