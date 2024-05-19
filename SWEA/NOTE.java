f25293a71891be90ebc48493b906939bd8298a5e
chanyoungit <uniti0903@naver.com>
2024-05-19 14:05:07 +0900

    commit message

--- SWEA/NOTE.java
+++ SWEA/NOTE.java
@@ -1,5 +1,17 @@
+import java.io.*;
+import java.util.*;
+
 class NOTE {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int T = sc.nextInt();
+        sc.nextLine();
+
+        int N =sc.nextInt();
+        sc.nextLine();
+
+        String str = sc.nextLine();
+        System.out.println(str);
     }
 }
