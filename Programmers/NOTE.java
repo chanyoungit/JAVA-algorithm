9fca3692795f9a66c2b8eb08526a497d99e4e5b9
chanyoungit <uniti0903@naver.com>
2024-05-27 23:15:42 +0900

    commit message

--- Programmers/NOTE.java
+++ Programmers/NOTE.java
@@ -1,7 +1,10 @@
+import java.io.*;
 import java.util.*;
 
 class NOTE {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
     }
 }
